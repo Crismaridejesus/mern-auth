a7bf54fbecc2cfcde02c45c63f562907b39aa3a7
Maricris De Jesus <maricris052298dejesus@gmail.com>
2025-08-04 16:48:14 +0800

    add routes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signup from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 
 function App() {
 
 
   return (
-    <>
-      <h1 className="flex items-center justify-center h-screen">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
