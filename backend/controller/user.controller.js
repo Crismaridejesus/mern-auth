0b5a0c9afa53b08dd0289c667d381221b551cd8e
Maricris De Jesus <maricris052298dejesus@gmail.com>
2025-08-06 20:22:13 +0800

    create test API route

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,4 @@
+export const test = (req, res) => {
+	// Logic to get user profile
+	res.json({ message: 'API is working' });
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -10,6 +11,9 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 });
 
 
+
 app.listen(3000, () => {
 	console.log('server is listening on port 3000');
-}); 
+});
+
+app.use('/api/users', userRoutes);
